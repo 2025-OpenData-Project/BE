cfe92f706786fa128595c768f7350d40d6573a7c
웃기진않아 <dugudada01@naver.com>
2025-04-01 15:38:00 +0900

    FIX: upsert 커스텀 로직

--- src/main/java/com/opendata/domain/apidata/repository/custom/CustomAreaRepository.java
+++ src/main/java/com/opendata/domain/apidata/repository/custom/CustomAreaRepository.java
@@ -1,4 +1,10 @@
 package com.opendata.domain.apidata.repository.custom;
 
-public interface CustomAreaRepository {
+import com.opendata.domain.apidata.entity.Area;
+
+import java.util.List;
+
+public interface CustomAreaRepository
+{
+    public void upsertAreaList(List<Area>areas);
 }
