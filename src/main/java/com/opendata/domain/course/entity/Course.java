e306ef4391fb0578e0bb6504f1097072f2647f63
Jinyoung <glitt5384@naver.com>
2025-04-06 19:53:39 +0900

    FEAT: 코스 엔티티

---
+++ src/main/java/com/opendata/domain/course/entity/Course.java
@@ -0,0 +1,22 @@
+package com.opendata.domain.course.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "courses")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    @Id
+    private String id;
+    private List<String> places;
+    private int placeCount;
+    private String startTime;
+    private String endTime;
+}
