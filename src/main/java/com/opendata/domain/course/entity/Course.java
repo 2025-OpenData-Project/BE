82c411eb2b234017537993f6f3b63f3209743026
웃기진않아 <dugudada01@naver.com>
2025-04-08 16:15:57 +0900

    FEAT: userId 추가

--- src/main/java/com/opendata/domain/course/entity/Course.java
+++ src/main/java/com/opendata/domain/course/entity/Course.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class Course {
     @Id
     private String id;
+    private String userId;
     private List<String> places;
     private int placeCount;
     private String startTime;
