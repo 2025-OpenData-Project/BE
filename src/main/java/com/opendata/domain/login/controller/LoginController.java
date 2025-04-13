6d797131c77b760aafb687c771b33b4bcfcf3b37
Jinyoung <glitt5384@naver.com>
2025-04-13 10:39:13 +0900

    DELETE: csrf 토큰 api 제거

--- src/main/java/com/opendata/domain/login/controller/LoginController.java
+++ src/main/java/com/opendata/domain/login/controller/LoginController.java
@@ -14,13 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(value = "http://localhost:63342")
 public class LoginController {
 
-    // 로그인 전 해당 경로로 요청을 보내 토큰 획득
-    @GetMapping("/csrf-token")
-    public String getCsrfToken(HttpServletRequest request) {
-        CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
-        return csrfToken != null ? csrfToken.getToken() : "CSRF token not found";
-    }
-
     @PostMapping("/login")
     public ResponseEntity<String> doLogin(){
         return ResponseEntity.ok().build();
