3daa83c4a4bbdeb6ca980b109d4b0a49988fed01
Jinyoung <glitt5384@naver.com>
2025-04-13 10:20:06 +0900

    REFACTOR: 어노테이션 * 로 간소화

--- src/main/java/com/opendata/domain/apidata/entity/Area.java
+++ src/main/java/com/opendata/domain/apidata/entity/Area.java
@@ -1,10 +1,7 @@
 package com.opendata.domain.apidata.entity;
 
 import com.opendata.domain.apidata.dto.CityDataDto;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,6 +10,7 @@ import java.util.List;
 @Document(collection = "area")
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Data
 public class Area
 {
