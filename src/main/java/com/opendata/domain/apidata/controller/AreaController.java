f4f21737e3d3ff875b50c50c33eb85ebc5d7acc0
Jinyoung <glitt5384@naver.com>
2025-05-02 21:02:54 +0900

    FEAT: 랜딩 페이지에서 관광지명과 혼잡도를 반환할 때 사용되는 API

--- src/main/java/com/opendata/domain/apidata/controller/AreaController.java
+++ src/main/java/com/opendata/domain/apidata/controller/AreaController.java
@@ -1,11 +1,15 @@
 package com.opendata.domain.apidata.controller;
 
+import com.opendata.domain.apidata.dto.AreaCongestionDto;
 import com.opendata.domain.apidata.service.AreaService;
+import com.opendata.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +23,10 @@ public class AreaController
     {
         areaService.fetchAllAreaAndSave();
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<ApiResponse<List<AreaCongestionDto>>> getAreaListWithCongestion(){
+        return ResponseEntity.ok(ApiResponse.onSuccess(areaService.fetchAndConvertAreaCongestionDto()));
     }
 }
