c19d239581a280a09c14113bdcf2285a9b930806
Jinyoung <glitt5384@naver.com>
2025-07-05 18:46:14 +0900

    fix: mysql 엔티티로 수정

--- src/main/java/com/opendata/domain/course/entity/Course.java
+++ src/main/java/com/opendata/domain/course/entity/Course.java
@@ -1,29 +1,30 @@
-//package com.opendata.domain.course.entity;
-//
-//import com.opendata.domain.tourspot.dto.AreaComponentDto;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.*;
-//
-//import java.util.List;
-//
-//@Entity
-//@Builder
-//@Getter
-//@Setter
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
-//@AllArgsConstructor(access = AccessLevel.PRIVATE)
-//public class Course {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private String id;
-//    private String userId;
-//    private List<AreaComponentDto> places;
-//    private int placeCount;
-//    private String startTime;
-//    private String endTime;
-//    private boolean isLike;
-//    private boolean isActive;
-//}
+package com.opendata.domain.course.entity;
+
+import com.opendata.domain.tourspot.dto.AreaComponentDto;
+import com.opendata.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "course")
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Course extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @OneToMany(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+    //private User userId;
+
+    @Builder.Default
+    private boolean isLike = false;
+
+    @Builder.Default
+    private boolean isActive = false;
+}
