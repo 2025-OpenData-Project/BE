4ea80069aa2ff7ca852c3ae7b72f08641d791576
Jinyoung <glitt5384@naver.com>
2025-07-05 18:43:03 +0900

    fix: 공통 코드 테이블 삭제, enum으로 관리

--- src/main/java/com/opendata/global/commoncode/repository/CommonCodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.opendata.global.commoncode.repository;
-
-import com.opendata.global.commoncode.entity.CommonCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommonCodeRepository extends JpaRepository<CommonCode, Long> {
-}
