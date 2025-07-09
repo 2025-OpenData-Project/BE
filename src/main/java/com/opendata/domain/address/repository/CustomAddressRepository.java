93fc2625f2ff5d3c9fc8cccdbd92e7215d68263c
Jinyoung <glitt5384@naver.com>
2025-07-10 04:15:57 +0900

    fix: 당장 동적 쿼리가 없어서 제거

--- src/main/java/com/opendata/domain/address/repository/CustomAddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.opendata.domain.address.repository;
-
-import com.opendata.domain.address.entity.Address;
-
-public interface CustomAddressRepository {
-    Address findByAddressKorNm(String addressKorNm);
-}
