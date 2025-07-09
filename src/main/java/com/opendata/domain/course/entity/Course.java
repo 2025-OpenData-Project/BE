1dd85228456a1430609326000557d1702afd406d
Jinyoung <glitt5384@naver.com>
2025-07-09 19:49:51 +0900

    fix: 테스트 도중 주석 처리

--- src/main/java/com/opendata/domain/course/entity/Course.java
+++ src/main/java/com/opendata/domain/course/entity/Course.java
@@ -1,11 +1,11 @@
 package com.opendata.domain.course.entity;
 
-import com.opendata.domain.tourspot.dto.AreaComponentDto;
 import com.opendata.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.List;
+import java.time.LocalDateTime;
+
 
 @Entity
 @Table(name = "course")
@@ -21,6 +21,13 @@ public class Course extends BaseEntity {
 //    @OneToMany(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "user_id")
     //private User userId;
+
+
+//    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+//    private List<CourseComponent> courseComponents;
+
+    private LocalDateTime startDtm;
+    private LocalDateTime endDtm;
 
     @Builder.Default
     private boolean isLike = false;
