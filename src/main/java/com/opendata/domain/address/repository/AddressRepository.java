c5d383e0d4237efc4cefedfb70e4a3c7ea49b19d
Jinyoung <glitt5384@naver.com>
2025-07-10 04:16:54 +0900

    fix: 쿼리 제거

--- src/main/java/com/opendata/domain/address/repository/AddressRepository.java
+++ src/main/java/com/opendata/domain/address/repository/AddressRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressRepository extends JpaRepository<Address, Long>, CustomAddressRepository {
-    Address findByAddressKorNm(String addressKorNm);
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
 }
