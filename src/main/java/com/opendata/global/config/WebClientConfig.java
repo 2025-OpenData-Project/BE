320b8220430f00cfd67ab608a74c7f002d378de7
7ijin01 <dugudada01@naver.com>
2025-07-13 20:15:41 +0900

    feat: WebClient bean 등록

---
+++ src/main/java/com/opendata/global/config/WebClientConfig.java
@@ -0,0 +1,21 @@
+package com.opendata.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient openApiWebClient() {
+        return WebClient.builder()
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(config -> config
+                                .defaultCodecs()
+                                .maxInMemorySize(1024 * 1024)) // 1MB
+                        .build())
+                .build();
+    }
+}
