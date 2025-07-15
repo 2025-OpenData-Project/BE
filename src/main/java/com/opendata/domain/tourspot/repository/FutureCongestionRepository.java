3062c92e8160c4338749ffa7119d7dfcac3972e0
Jinyoung <glitt5384@naver.com>
2025-07-15 23:34:51 +0900

    chore: 디렉토리 변경

--- src/main/java/com/opendata/domain/tourspot/repository/FutureCongestionRepository.java
+++ src/main/java/com/opendata/domain/tourspot/repository/FutureCongestionRepository.java
@@ -1,6 +1,7 @@
 package com.opendata.domain.tourspot.repository;
 
 import com.opendata.domain.tourspot.entity.TourSpotFutureCongestion;
+import com.opendata.domain.tourspot.repository.custom.CustomFutureCongestionRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
