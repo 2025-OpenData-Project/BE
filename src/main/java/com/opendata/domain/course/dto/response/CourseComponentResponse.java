40ff2b53670239ce71c3f94dc038ff5426ae4b65
Jinyoung <glitt5384@naver.com>
2025-07-31 19:17:20 +0900

    feat: 위도 경도 추가, 시간 포매팅

--- src/main/java/com/opendata/domain/course/dto/response/CourseComponentResponse.java
+++ src/main/java/com/opendata/domain/course/dto/response/CourseComponentResponse.java
@@ -1,11 +1,19 @@
 package com.opendata.domain.course.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mysql.cj.util.TimeUtil;
 import com.opendata.domain.course.entity.CourseComponent;
+import com.opendata.domain.tourspot.entity.TourSpot;
+import com.opendata.global.util.DateUtil;
 
 import java.time.LocalDateTime;
 
-public record CourseComponentResponse(String tourspotNm, LocalDateTime time) {
+public record CourseComponentResponse(String tourspotNm,
+                                      @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                                      LocalDateTime time, Double lat, Double lon) {
     public static CourseComponentResponse from(CourseComponent c) {
-        return new CourseComponentResponse(c.getTourSpot().getTourspotNm(), c.getTourspotTm());
+        TourSpot tourSpot = c.getTourSpot();
+        return new CourseComponentResponse(tourSpot.getTourspotNm(), c.getTourspotTm(),
+                tourSpot.getAddress().getLatitude(), tourSpot.getAddress().getLongitude());
     }
 }
