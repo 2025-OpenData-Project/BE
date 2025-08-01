249d3534d540239396179b3d2b7732fea24ff692
7ijin01 <dugudada01@naver.com>
2025-08-01 22:31:13 +0900

    fix: getAttributes() HashMap리턴 수정

--- src/main/java/com/opendata/domain/oauth2/dto/user/CustomOAuth2User.java
+++ src/main/java/com/opendata/domain/oauth2/dto/user/CustomOAuth2User.java
@@ -6,6 +6,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -16,7 +17,8 @@ public class CustomOAuth2User implements OAuth2User
     @Override
     public Map<String, Object> getAttributes() {
 
-        return null;
+        return new HashMap<>();
+
     }
 
     @Override
