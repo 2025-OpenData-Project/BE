13a00a1364d2edb7bf80caae7ae1df56a7b57608
Jinyoung <glitt5384@naver.com>
2025-08-03 07:02:15 +0900

    feat: 현재 시간으로 혼잡도 조회 쿼리 추가

---
+++ src/main/java/com/opendata/domain/tourspot/repository/custom/CustomCurrentCongestionRepository.java
@@ -0,0 +1,8 @@
+package com.opendata.domain.tourspot.repository.custom;
+
+import com.opendata.domain.tourspot.entity.TourSpot;
+import com.opendata.domain.tourspot.entity.TourSpotCurrentCongestion;
+
+public interface CustomCurrentCongestionRepository {
+    TourSpotCurrentCongestion findByTourSpotAndCurTime(TourSpot tourSpot, String fsctTime);
+}
