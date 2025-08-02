1d5c6a2926c9e69bca9b9b8b94d6643422d25a6c
Jinyoung <glitt5384@naver.com>
2025-08-03 07:05:08 +0900

    feat: 관광지 이벤트 전체 조회 쿼리 추가

--- src/main/java/com/opendata/domain/tourspot/repository/custom/CustomTourSpotEventRepository.java
+++ src/main/java/com/opendata/domain/tourspot/repository/custom/CustomTourSpotEventRepository.java
@@ -1,5 +1,11 @@
 package com.opendata.domain.tourspot.repository.custom;
 
+import com.opendata.domain.tourspot.entity.TourSpot;
+import com.opendata.domain.tourspot.entity.TourSpotEvent;
+
+import java.util.List;
+
 public interface CustomTourSpotEventRepository {
     boolean existsByEventNameAndEventPeriod(String eventName, String eventPeriod);
+    List<TourSpotEvent> findAllByTourSpot(TourSpot tourSpot);
 }
