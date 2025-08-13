6ff7c32ea38c62d301f22ec95d836dda97e0e07e
7ijin01 <dugudada01@naver.com>
2025-08-13 20:01:41 +0900

    feat: swagger accesstoken 적용

--- src/main/java/com/opendata/global/config/SwaggerConfig.java
+++ src/main/java/com/opendata/global/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.opendata.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +14,32 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        Info info = new Info()
-                .version("v1.0")
-                .title("OpenData API");
+        // Security Scheme 정의 (Bearer JWT)
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization"); // 헤더 이름
+
+        // Security Requirement (전역 적용)
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Authorization");
+
+        // Components에 Security Scheme 등록
+        Components components = new Components()
+                .addSecuritySchemes("Authorization", apiKey);
+
         return new OpenAPI()
-                .info(info);
+                .addServersItem(new Server().url("/")) // 리버스 프록시 등일 때 베이스 경로
+                .components(components)
+                .addSecurityItem(securityRequirement)
+                .info(apiInfo());
     }
 
+    private Info apiInfo() {
+        return new Info()
+                .title("OpenData API")
+                .version("v1.0");
+    }
 }
