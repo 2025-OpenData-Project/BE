e09fad9081dd8f644003df0a95be3ddca8f40f3a
7ijin01 <dugudada01@naver.com>
2025-08-13 20:02:18 +0900

    feat: CustomCourseComponentRepository 상속

--- src/main/java/com/opendata/domain/course/repository/CourseComponentRepository.java
+++ src/main/java/com/opendata/domain/course/repository/CourseComponentRepository.java
@@ -1,9 +1,14 @@
 package com.opendata.domain.course.repository;
 
 import com.opendata.domain.course.entity.CourseComponent;
+import com.opendata.domain.course.repository.custom.CustomCourseComponentRepository;
+import com.opendata.domain.course.repository.custom.CustomCourseRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CourseComponentRepository extends JpaRepository<CourseComponent, Long> {
+public interface CourseComponentRepository extends JpaRepository<CourseComponent, Long> , CustomCourseComponentRepository {
+
 }
