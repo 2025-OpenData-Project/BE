d54995c833d4c619dde4a14ac0ab93a2e737423b
7ijin01 <dugudada01@naver.com>
2025-08-13 20:00:25 +0900

    fix: N+1 문제 해결

--- src/main/java/com/opendata/domain/course/repository/custom/CustomCourseRepository.java
+++ src/main/java/com/opendata/domain/course/repository/custom/CustomCourseRepository.java
@@ -1,6 +1,10 @@
 package com.opendata.domain.course.repository.custom;
 
 
-public interface CustomCourseRepository {
+import com.opendata.domain.course.entity.Course;
 
+import java.util.List;
+
+public interface CustomCourseRepository {
+    List<Course> findAllByUserId(Long userId);
 }
--- src/main/java/com/opendata/domain/course/repository/custom/CustomCourseRepositoryImpl.java
+++ src/main/java/com/opendata/domain/course/repository/custom/CustomCourseRepositoryImpl.java
@@ -1,6 +1,11 @@
 package com.opendata.domain.course.repository.custom;
 
 
+import com.opendata.domain.course.entity.Course;
+import com.opendata.domain.course.entity.QCourse;
+import com.opendata.domain.course.entity.QCourseComponent;
+import com.opendata.domain.tourspot.entity.QTourSpot;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -8,4 +13,21 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CustomCourseRepositoryImpl implements CustomCourseRepository{
 
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Course> findAllByUserId(Long userId) {
+        QCourse c = QCourse.course;
+        QCourseComponent cc = QCourseComponent.courseComponent;
+        QTourSpot ts = QTourSpot.tourSpot;
+
+        return queryFactory
+                .selectDistinct(c)
+                .from(c)
+                .leftJoin(c.courseComponents, cc).fetchJoin()
+                .leftJoin(cc.tourSpot, ts).fetchJoin()
+                .where(c.user.id.eq(userId))
+                .orderBy(c.startDtm.desc(), cc.tourspotTm.asc())
+                .fetch();
+    }
 }
