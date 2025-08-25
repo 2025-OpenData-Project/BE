a71b1e68bfb8e39fd695e691d46bf88dc34e291b
Jinyoung <glitt5384@naver.com>
2025-08-26 07:01:29 +0900

    feat: 주소 기준 연관 관광지 Dto 반환 쿼리

--- src/main/java/com/opendata/domain/tourspot/repository/custom/related/CustomTourSpotRelatedRepositoryImpl.java
+++ src/main/java/com/opendata/domain/tourspot/repository/custom/related/CustomTourSpotRelatedRepositoryImpl.java
@@ -1,10 +1,33 @@
 package com.opendata.domain.tourspot.repository.custom.related;
 
+import com.opendata.domain.address.entity.Address;
+import com.opendata.domain.tourspot.dto.response.TourSpotRelatedResponse;
+import com.opendata.domain.tourspot.entity.QTourSpotRelated;
+import com.opendata.domain.tourspot.entity.TourSpotRelated;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 @RequiredArgsConstructor
-public class CustomTourSpotRelatedRepositoryImpl implements CustomTourSpotRelatedRepository
-{
+public class CustomTourSpotRelatedRepositoryImpl implements CustomTourSpotRelatedRepository {
+    private final JPAQueryFactory jpaQueryFactory;
+
+
+    @Override
+    public Optional<List<TourSpotRelatedResponse>> findAllByAddress(Address address) {
+        QTourSpotRelated qTourSpotRelated = QTourSpotRelated.tourSpotRelated;
+
+        return Optional.ofNullable(jpaQueryFactory.select(Projections.constructor(TourSpotRelatedResponse.class,
+                        qTourSpotRelated.id, qTourSpotRelated.tourSpotCode, qTourSpotRelated.tourSpotName,
+                        qTourSpotRelated.largeCategory, qTourSpotRelated.middleCategory,
+                        qTourSpotRelated.mapX, qTourSpotRelated.mapY))
+                        .from(qTourSpotRelated)
+                .where(qTourSpotRelated.address.eq(address))
+                .fetch());
+    }
 }
