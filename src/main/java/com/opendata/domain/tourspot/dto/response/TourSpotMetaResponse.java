405182a8da1a20e4e8db8bbea9c8e47347a034e7
Jinyoung <glitt5384@naver.com>
2025-09-02 17:21:20 +0900

    feat: CongestionLevel -> String

--- src/main/java/com/opendata/domain/tourspot/dto/response/TourSpotMetaResponse.java
+++ src/main/java/com/opendata/domain/tourspot/dto/response/TourSpotMetaResponse.java
@@ -1,5 +1,6 @@
 package com.opendata.domain.tourspot.dto.response;
 
+import com.opendata.domain.tourspot.entity.enums.CongestionLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,4 +11,11 @@ public class TourSpotMetaResponse {
     private String tourspotNm;
     private String imageUrl;
     private String congestionLabel;
+
+    public TourSpotMetaResponse(Long id, String name, String imageUrl, CongestionLevel congestionLvl) {
+        this.tourspotId = id;
+        this.tourspotNm = name;
+        this.imageUrl = imageUrl;
+        this.congestionLabel = congestionLvl.getCongestionLabel();
+    }
 }
