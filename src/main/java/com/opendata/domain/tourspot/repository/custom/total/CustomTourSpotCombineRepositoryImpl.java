a9c492a3dbc228934272ceeeb7efa46841eaca83
Jinyoung <glitt5384@naver.com>
2025-09-02 17:22:18 +0900

    feat: 응답 시간 줄이기 위해 프로젝션으로 쿼리 한 번만 보내기

---
+++ src/main/java/com/opendata/domain/tourspot/repository/custom/total/CustomTourSpotCombineRepositoryImpl.java
@@ -0,0 +1,55 @@
+package com.opendata.domain.tourspot.repository.custom.total;
+
+import com.opendata.domain.tourspot.dto.response.TourSpotMetaResponse;
+import com.opendata.domain.tourspot.entity.QTourSpot;
+import com.opendata.domain.tourspot.entity.QTourSpotCurrentCongestion;
+import com.opendata.domain.tourspot.entity.QTourSpotImage;
+import com.opendata.global.util.DateUtil;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CustomTourSpotCombineRepositoryImpl implements CustomTourSpotCombineRepository {
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<TourSpotMetaResponse> findMetaPage(Pageable pageable) {
+        QTourSpot s = QTourSpot.tourSpot;
+        QTourSpotImage img = QTourSpotImage.tourSpotImage;
+        QTourSpotCurrentCongestion cc = QTourSpotCurrentCongestion.tourSpotCurrentCongestion;
+
+        return queryFactory
+                .select(Projections.constructor(TourSpotMetaResponse.class,
+                        s.tourspotId,
+                        s.tourspotNm,
+                        img.tourspotImgUrl,
+                        cc.congestionLvl
+                ))
+                .from(s)
+                .leftJoin(img).on(img.tourspot.eq(s))
+                .leftJoin(cc).on(cc.tourspot.eq(s)
+                        .and(cc.fcstTime.eq(DateUtil.getCurrentRoundedFormattedDateTime())))
+                .orderBy(s.tourspotId.asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    @Override
+    public long countAll() {
+        QTourSpot s = QTourSpot.tourSpot;
+
+        Long count = queryFactory
+                .select(s.count())
+                .from(s)
+                .fetchOne();
+
+        return count == null ? 0 : count;
+    }
+}
