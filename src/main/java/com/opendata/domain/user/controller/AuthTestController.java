70a8cfeaff4dc97d62df545794b2dfd4356c5a7e
7ijin01 <dugudada01@naver.com>
2025-09-12 18:17:46 +0900

    feat: 로그인 테스트

--- src/main/java/com/opendata/domain/user/controller/AuthTestController.java
+++ src/main/java/com/opendata/domain/user/controller/AuthTestController.java
@@ -4,13 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.opendata.domain.user.service.UserService;
 import com.opendata.global.jwt.JwtUtil;
 import com.opendata.global.response.ApiResponse;
+import com.opendata.global.security.CustomUserDetails;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/auth")
 public class AuthTestController {
 	private final JwtUtil jwtUtil;
+	private final UserService userService;
 
 	@PostMapping
 	public ResponseEntity<ApiResponse<Map<String, String>>> createDevToken(@RequestParam String email) {
@@ -30,5 +35,17 @@ public class AuthTestController {
 		tokens.put("refresh", refreshToken);
 
 		return ResponseEntity.ok(ApiResponse.onSuccess(tokens));
+	}
+
+	@GetMapping("/test")
+	public ResponseEntity<ApiResponse<Boolean>> test(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+		try {
+			userService.findUserByEmailOrThrow(customUserDetails);
+			return ResponseEntity.ok(ApiResponse.onSuccess(true));
+		} catch (Exception e) {
+			return ResponseEntity.ok(ApiResponse.onSuccess(false));
+		}
+
+
 	}
 }
