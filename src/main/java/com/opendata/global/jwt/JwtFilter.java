c60a76cc8cade15f01152d291b6f607a1035c5f9
Jinyoung <glitt5384@naver.com>
2025-09-15 00:00:27 +0900

    feat: 요청에서 쿠키유틸로 토큰 파싱

--- src/main/java/com/opendata/global/jwt/JwtFilter.java
+++ src/main/java/com/opendata/global/jwt/JwtFilter.java
@@ -12,11 +12,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
@@ -26,6 +28,26 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Optional<String> accessTokenOpt = CookieUtil.getAccessTokenFromRequest(request);
+        Optional<String> refreshTokenOpt = CookieUtil.getRefreshTokenFromRequest(request);
+
+        if (accessTokenOpt.isPresent()) {
+            String token = accessTokenOpt.get();
+            if (!jwtUtil.isExpired(token)) {
+                String email = jwtUtil.getEmail(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+        }
+
+
         String accessToken = null;
 
         String header = request.getHeader("Authorization");
